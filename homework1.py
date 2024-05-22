d0be2b37e3a15556e757dcc83b13b4f16ab8da57
Pasa74R <rgo-rko@mail.ru>
2024-05-22 09:57:20 +0300

    Add files via upload

---
+++ homework1.py
@@ -0,0 +1,6 @@
+example = 'Дратути'
+print(example [0])
+print(example [-1])
+print(example [4:]) # половина не выходит для нечётности, взял таким образом.
+print(example [::-1])
+print(example [1:6:2])
